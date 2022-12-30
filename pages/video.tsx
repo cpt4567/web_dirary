cf9bbfc086f7d6bbd40777990ade32babd1bff3f
cpt4567 <cpt4567@gmail.com>
2022-12-30 18:39:49 +0900

    영상 재생 및 저장 다운로드 구현

--- pages/video.tsx
+++ pages/video.tsx
@@ -1,12 +1,16 @@
-import { useCallback, useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function Vedeo() {
     
     const videoElement = useRef< HTMLVideoElement | null >( null )
+    
+    const [videoEvent,setVideoEvent] = useState<any> ()
+
+    const [test, setTest] = useState<any[]>([])
 
     const option = {
         autoPlay : true,
-        style:{ width:300 , height:300 }
+        style:{ width:1200 , height:500 }
     }
 /* 
 
@@ -25,43 +29,38 @@ https://curryyou.tistory.com/443
      * @url https://developer.mozilla.org/en-US/docs/Web/API/Navigator
      *   
      * */ 
-   /*  useEffect(() => {
-        // 보안 컨텍스트: 이 기능은 일부 또는 모든 지원 브라우저 에서 보안 컨텍스트 (HTTPS) 에서만 사용할 수 있습니다 . 
-        // 이 메서드 는 요청된 미디어 유형을 포함하는 트랙 을 생성하는 미디어 입력을 사용할 수 있는 권한을 사용자에게 요청합니다
-
-        let stream = new MediaStream() ;
-    
-        let source = new MediaSource();
-    
-      }, [videoElement]) */
-    
-      
-/* 
-      const onReadCam = async () => {
-
-        try {
-             
-            const stream = await navigator.mediaDevices.getUserMedia( { video : true , audio : true } );
-                
-                if(!!videoElement && !!videoElement.current && !!videoElement.current.srcObject)
-
-                    videoElement.current.srcObject = stream
-
-        } 
-        catch (err) { 
-            console.log(err);
-        }
-
-    }     */
 
     const onReadCam = async () => {
+// 보안 컨텍스트: 이 기능은 일부 또는 모든 지원 브라우저 에서 보안 컨텍스트 (HTTPS) 에서만 사용할 수 있습니다 . 
+        // 이 메서드 는 요청된 미디어 유형을 포함하는 트랙 을 생성하는 미디어 입력을 사용할 수 있는 권한을 사용자에게 요청합니다
 
         try {     
-            const stream = await navigator.mediaDevices.getUserMedia( { video : true , audio : true } );
+            const stream = await navigator.mediaDevices.getUserMedia( { video : true , audio : false } );
                 
             if(videoElement.current){
                 
                 videoElement.current.srcObject = stream ;
+                
+                let reader = new MediaRecorder(stream);
+                
+                reader.ondataavailable = (event)=>{
+                    console.log(event);
+                    
+                    // 녹화 데이터(Blob)가 들어올 때마다 배열에 담아두기
+                    setTest( test.concat(event.data) );
+                }             
+                reader.onstop = (event)=>{
+                    // 배열에 담아둔 녹화 데이터들을 통합한 Blob객체 생성
+                    const videoBlob = new Blob(test);
+
+                }
+
+                setVideoEvent( reader )
+
+                videoElement.current.onloadedmetadata = () => {
+                    videoElement.current?.play();
+                }
+                                
             }
 
         } 
@@ -73,16 +72,51 @@ https://curryyou.tistory.com/443
     }
 
     useEffect(() => {
-        /* if (navigator.mediaDevices.getUserMedia) */
-            onReadCam()
+        onReadCam();        
     }, [])
+    
+
+
+    const onStart = () => {
+
+        videoEvent.start()        
+    }
+
+    const onEnd = () => {
+        videoEvent.stop()
+
+    }    
+
+    const onDownload = ()=>{
+
+        let downloadDom = document.createElement("a")
+
+        const downloadfile = new Blob([test[0]],{type:"video/x-matroska;codecs=avc1"})
+
+        const downloadUrl = window.URL.createObjectURL(downloadfile); // 해당 file을 가리키는 url 생성
+        
+        downloadDom.href = downloadUrl
+
+        downloadDom.click()
+        
+    }
+
+
+    useEffect(() => {
+        console.log(test,"데이터");
+
+    }, [test])
+    
     
 
     return (
         <>
         <video { ...option } ref={ videoElement } />
-{/*         <button onClick={onReadCam}>assaas</button>
- */}        </>
+        <button onClick={onStart}>start</button>
+        <button onClick={onEnd}>end</button>
+        <button onClick={onDownload}>download</button>
+
+        </>
     )
       
 }
